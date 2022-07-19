7311b38a058f535eeb0520e93b47aec2c92beedc
Trinidad Boragini <mtboragini@gmail.com>
2022-07-18 21:40:19 -0300

    Instal reactstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 
